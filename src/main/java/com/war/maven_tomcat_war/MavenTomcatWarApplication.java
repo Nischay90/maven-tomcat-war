6535c3ae6794772cab99081c142eab509d69874f
Nischay90 <nischayhosanna90@gmail.com>
2024-07-02 15:45:19 +0530

    first commit

---
+++ src/main/java/com/war/maven_tomcat_war/MavenTomcatWarApplication.java
@@ -0,0 +1,13 @@
+package com.war.maven_tomcat_war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenTomcatWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenTomcatWarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/war/maven_tomcat_war/MavenTomcatWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.war.maven_tomcat_war;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenTomcatWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
